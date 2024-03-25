e88a26b904ba534c8e40fa4aa176709d6656930e
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-03-25 04:12:17 +0200

    JWT Layers and reset password

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 const path = require("path");
 const fs = require("fs");
 const ejs = require("ejs");
+const { error } = require("console");
 
 async function userSignupController(req, res) {
   try {
@@ -73,8 +74,8 @@ async function userSignupController(req, res) {
     await transporter.sendMail(message).catch((err) => {
       throw err;
     });
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRE,
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_V3, {
+      expiresIn: process.env.JWT_EXPIRE_V3,
     });
     const userWithoutPassword = { ...user };
     delete userWithoutPassword._doc.password;
@@ -141,7 +142,10 @@ async function resendOTP(req, res) {
     await transporter.sendMail(message).catch((err) => {
       throw err;
     });
-    res.status(201).json({ msg: "code sent.." });
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_V3, {
+      expiresIn: process.env.JWT_EXPIRE_V3,
+    });
+    res.status(201).json({ msg: "code sent..", token: token });
   } catch (error) {
     console.log(error);
     return res.status(500).json("INTERNAL SERVER ERROR");
@@ -196,8 +200,8 @@ async function loginController(req, res) {
     if (!user.verified) {
       return res.status(401).json("Account is not verified");
     }
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRE,
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_V2, {
+      expiresIn: process.env.JWT_EXPIRE_V2,
     });
     await OTP.deleteMany({ email: user.email });
     let otp = Math.floor(1000 + Math.random() * 9000);
@@ -282,12 +286,129 @@ async function verifyLogin(req, res) {
   }
 }
 
+async function forgotPasswordRequestController(req, res) {
+  try {
+    if (!req.body.email) {
+      return res.status(422).json("Invalid Credentials");
+    }
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json("User not found");
+    }
+    await OTP.deleteMany({ email: user.email });
+    let otp = Math.floor(1000 + Math.random() * 9000);
+    const d = new Date();
+    d.setMinutes(d.getMinutes());
+    const d2 = new Date();
+    d2.setMinutes(d2.getMinutes() + 1);
+
+    let OTP_Obj = new OTP({
+      code: await bcrypt.hash(String(otp), 10),
+      email: user.email,
+      createdAt: Number(d),
+      expiresAt: Number(d2),
+    });
+    await OTP_Obj.save();
+
+    const data = {
+      name: user.name,
+      email: user.email,
+      otp: otp,
+    };
+    const filePath = path.join(
+      __dirname,
+      "..",
+      "public",
+      "mail-template",
+      "index2.ejs"
+    );
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    const modifiedEmailTemplate = ejs.render(fileContent, data);
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      service: "Gmail",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASSWORD,
+      },
+    });
+    let message = {
+      from: "Training Center",
+      to: user.email,
+      subject: "Change Password!",
+      html: modifiedEmailTemplate,
+    };
+    await transporter.sendMail(message).catch((err) => {
+      throw err;
+    });
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_V4, {
+      expiresIn: process.env.JWT_EXPIRE_V4,
+    });
+    res.status(201).json({ msg: "code sent..", token: token });
+  } catch (error) {
+    return res.status(500).json("INTERNAL SERVER ERROR");
+  }
+}
+
+async function verifyForgotPassword(req, res) {
+  try {
+    if (!req.body.otp) {
+      return res.status(401).json("Must provide otp..");
+    }
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ msg: "User not found." });
+    }
+    const otp = await OTP.findOne({ email: user.email });
+    if (!otp) {
+      return res.status(404).json({ msg: "No OTP was sent." });
+    }
+    if (!(await bcrypt.compare(req.body.otp, otp.code))) {
+      return res.status(401).json({ msg: "Wrong code." });
+    }
+    let d = new Date();
+    if (Number(d) > Number(otp.expiresAt)) {
+      return res.status(400).json({ msg: "OTP has expired." });
+    }
+    await OTP.deleteMany({ email: user.email });
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_V5, {
+      expiresIn: process.env.JWT_EXPIRE_V5,
+    });
+    return res.status(200).json({ msg: "Proceed to change your password", token: token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("INTERNAL SERVER ERROR");
+  }
+}
+
+async function changePassword(req, res) {
+  try {
+    if (!req.body.password) {
+      return res.status(422).json("Invalid Credentials");
+    }
+    const user = await User.findById(req.userId);
+    if (!user) {
+      throw error;
+    }
+    user.password = bcrypt.hashSync(req.body.password, 10);
+    await user.save();
+    return res.status(200).json("Password Changed Successfully");
+  } catch (error) {
+    return res.status(500).json("INTERNAL SERVER ERROR");
+  }
+}
+
 module.exports = {
   userSignupController,
   resendOTP,
   verifyUser,
   loginController,
   verifyLogin,
+  forgotPasswordRequestController,
+  verifyForgotPassword,
+  changePassword,
 };
 
 /**
--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,27 @@ const {
   verifyUser,
   loginController,
   verifyLogin,
+  changePassword,
+  forgotPasswordRequestController,
+  verifyForgotPassword,
 } = require("../controllers/userAuth");
 const UserPrivileges = require("../middlewares/protect");
+const UserPrivilegesV2 = require("../middlewares/protectV2");
+const UserPrivilegesV3 = require("../middlewares/protectV3");
+const UserPrivilegesV4 = require("../middlewares/protectV4");
+const UserPrivilegesV5 = require("../middlewares/protectV5");
 var router = express.Router();
 
 router.post("/login", loginController);
+router.post("/verifyLogin", UserPrivilegesV2, verifyLogin);
+
 router.post("/signup", userSignupController);
-router.post("/resendOTP", UserPrivileges, resendOTP);
-router.post("/verify", UserPrivileges, verifyUser);
-router.post("/verifyLogin", UserPrivileges, verifyLogin);
+router.post("/resendOTP", UserPrivilegesV3, resendOTP);
+router.post("/verify", UserPrivilegesV3, verifyUser);
+
+router.post("/requestPasswordReset", forgotPasswordRequestController);
+router.post("/verifyResetPassword",UserPrivilegesV4, verifyForgotPassword);
+router.post("/changePassword", UserPrivilegesV5, changePassword);
 
 /** MAIL TEMPLATE */
 // router.get("/", (req, res) => {
